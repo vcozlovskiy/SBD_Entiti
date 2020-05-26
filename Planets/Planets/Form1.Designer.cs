9ce9b5dfd0304637879aa8d5d98c0f3eeb4d3127
vcozlovskiy <38006998+vcozlovskiy@users.noreply.github.com>
2020-05-26 17:43:03 +0300

    Add files via upload

---
+++ Planets/Planets/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace Planets
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.planet_name = new System.Windows.Forms.TextBox();
+            this.planet_mass = new System.Windows.Forms.TextBox();
+            this.planet_radius = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.out_speed = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(24, 149);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(160, 24);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // planet_name
+            // 
+            this.planet_name.Location = new System.Drawing.Point(20, 41);
+            this.planet_name.Margin = new System.Windows.Forms.Padding(4);
+            this.planet_name.Multiline = true;
+            this.planet_name.Name = "planet_name";
+            this.planet_name.Size = new System.Drawing.Size(160, 24);
+            this.planet_name.TabIndex = 2;
+            // 
+            // planet_mass
+            // 
+            this.planet_mass.Location = new System.Drawing.Point(189, 41);
+            this.planet_mass.Margin = new System.Windows.Forms.Padding(4);
+            this.planet_mass.Name = "planet_mass";
+            this.planet_mass.Size = new System.Drawing.Size(132, 22);
+            this.planet_mass.TabIndex = 3;
+            // 
+            // planet_radius
+            // 
+            this.planet_radius.Location = new System.Drawing.Point(331, 41);
+            this.planet_radius.Margin = new System.Windows.Forms.Padding(4);
+            this.planet_radius.Name = "planet_radius";
+            this.planet_radius.Size = new System.Drawing.Size(132, 22);
+            this.planet_radius.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 15);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(189, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Имя новой/редакт планеты";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(209, 15);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Масса";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(327, 11);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(134, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Полярнаый радиус";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(20, 127);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(372, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Вычислить первую космическую для планеты/удалить:";
+            // 
+            // out_speed
+            // 
+            this.out_speed.Location = new System.Drawing.Point(24, 201);
+            this.out_speed.Margin = new System.Windows.Forms.Padding(4);
+            this.out_speed.Name = "out_speed";
+            this.out_speed.Size = new System.Drawing.Size(132, 22);
+            this.out_speed.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(20, 178);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 17);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Ответ :";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(24, 74);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(133, 49);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Сохранить планету";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(189, 73);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(173, 21);
+            this.checkBox1.TabIndex = 12;
+            this.checkBox1.Text = "сохранить изменения";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(212, 149);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 36);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Удалить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button1_MouseClick);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(293, 149);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(129, 36);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Редактировать";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button3_MouseClick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(24, 267);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 15;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(17, 247);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(145, 17);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Вторая космическая";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 306);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.out_speed);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.planet_radius);
+            this.Controls.Add(this.planet_mass);
+            this.Controls.Add(this.planet_name);
+            this.Controls.Add(this.comboBox1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox planet_name;
+        private System.Windows.Forms.TextBox planet_mass;
+        private System.Windows.Forms.TextBox planet_radius;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox out_speed;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Planets/Planets/Form1.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Entity;
+using System.Data.SqlClient;
+
+namespace Planets
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Planet[] temp1;
+        List<Planet> PlanetsList = new List<Planet>();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Planet Mercyriy = new Planet
+            {
+                Id = 1,
+                Name = "Mercyriy",
+                Radius = 24397000,
+                Mass = 3.33 * Math.Pow(10.0, 23.0)
+            };
+            Planet Venera = new Planet
+            {
+                Id = 2,
+                Name = "Venera",
+                Radius = 6051000,
+                Mass = 4.86 * Math.Pow(10.0, 24)
+            };
+            Planet Ertch = new Planet
+            {
+                Id = 3,
+                Name = "Ertch",
+                Radius = 6371000,
+                Mass = 5.972 * Math.Pow(10.0, 24)
+            };
+            Planet Mars = new Planet
+            {
+                Id = 4,
+                Name = "Mars",
+                Radius = 3396000,
+                Mass = 6.417 * Math.Pow(10.0, 23.0)
+            };
+            Planet Ypiter = new Planet
+            {
+                Id = 5,
+                Name = "Ypiter",
+                Radius = 69911000,
+                Mass = 1.89 * Math.Pow(10, 27)
+            };
+            Planet Saturn = new Planet
+            {
+                Id = 6,
+                Name = "Saturn",
+                Radius = 58388000,
+                Mass = 5.68 * Math.Pow(10, 26)
+            };
+            Planet Uran = new Planet
+            {
+                Id = 7,
+                Name = "Uran",
+                Radius = 25559000,
+                Mass = 8.681 * Math.Pow(10, 26)
+            };
+            Planet Neptun = new Planet
+            {
+                Id = 8,
+                Name = "Neptun",
+                Radius = 24662000,
+                Mass = 1.024 * Math.Pow(10, 26)
+            };
+            using (PlanetContext PC = new PlanetContext())
+            {
+                
+                temp1 = new Planet[] { Mercyriy, Venera, Ertch, Mars, Ypiter, Saturn, Uran, Neptun };
+                if (PC.Planets.ToList().Count < 8) 
+                {
+                    PC.Planets.AddRange(temp1);
+                    PC.SaveChanges();
+                }
+                PlanetsList = new List<Planet>();
+                List<Planet> PlanetsListNow = PC.Planets.ToList();
+                for (int i = 0; PC.Planets.ToList().Count() > PlanetsList.Count(); i++)
+                {
+                    PlanetsList.Add(PlanetsListNow[i]);
+                }
+            }
+            foreach (Planet p in PlanetsList)
+            {
+                comboBox1.Items.Add(p.Name);
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            using (PlanetContext P = new PlanetContext())
+            {
+                Planet WhatVel = PlanetsList[comboBox1.SelectedIndex];
+                out_speed.Text = WhatVel.CalcFirtCosmVel().ToString("F2") + ", М/С";
+                planet_name.Text = WhatVel.Name;
+                planet_mass.Text = WhatVel.Mass.ToString();
+                planet_radius.Text = WhatVel.Radius.ToString();
+                textBox1.Text = WhatVel.CalcSecndCosmVel().ToString("F2") + ", М/С";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (PlanetContext P = new PlanetContext())
+            {
+                double.TryParse(planet_mass.Text,out double mass);
+                double.TryParse(planet_radius.Text, out double rad);
+                Planet PlanetNew = new Planet()
+                {
+                    Id = (P.Planets.Count() + 1),
+                    Name = planet_name.Text,
+                    Mass = mass,
+                    Radius = rad
+                };
+                P.Planets.Add(PlanetNew);
+                if (checkBox1.Checked == true)
+                {
+                    P.SaveChanges();
+                    comboBox1.Items.Add(PlanetNew.Name);
+                    PlanetsList.Add(PlanetNew);
+                }
+            }
+        }
+
+        private void button1_MouseClick(object sender, MouseEventArgs e)
+        {
+            string connectionString = @"data source=DESKTOP-63OB1FE\MSSWL;Initial Catalog=PAL;Integrated Security=True;";
+            string sqlExpression = $"DELETE FROM Planets WHERE Name = '{comboBox1.SelectedItem.ToString()}'";
+            using (SqlConnection connection =new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand cmd = new SqlCommand(sqlExpression, connection);
+                cmd.ExecuteNonQuery();
+                comboBox1.SelectedIndex -= 1;
+            }
+            using (PlanetContext PC = new PlanetContext())
+            {
+                PlanetsList = new List<Planet>();
+                List<Planet> PlanetsListNow = PC.Planets.ToList();
+                comboBox1.Items.Clear();
+                for (int i = 0; PC.Planets.ToList().Count() - 1 > PlanetsList.Count(); i++)
+                {
+                    PlanetsList.Add(PlanetsListNow[i]);
+                    comboBox1.Items.Add(PlanetsListNow[i].Name);
+                }
+            }
+
+        }
+
+        private void button3_MouseClick(object sender, MouseEventArgs e)
+        {
+            using (PlanetContext P = new PlanetContext())
+            {
+                Planet p = P.Planets.ToList()[comboBox1.SelectedIndex];
+                planet_name.Text = p.Name;
+                planet_mass.Text = p.Mass.ToString();
+                planet_radius.Text = p.Radius.ToString();
+                if (planet_name.Text != null && planet_mass.Text != null && planet_radius.Text != null)
+                {
+                    p.Name = planet_name.Text;
+                    double m;
+                    double.TryParse(planet_mass.Text, out m);
+                    p.Mass = m;
+                    double.TryParse(planet_radius.Text, out m);
+                    p.Radius = m;
+                }
+                P.SaveChanges();
+
+
+            }
+        }
+    }
+    class PlanetContext : DbContext
+    {
+        public PlanetContext()
+            : base("DbConnection")
+        { }
+        public DbSet<Planet> Planets { get; set; }
+    }
+    public class Planet
+    {
+        private static double _GravConst = 6.67 * Math.Pow(10, -11);
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Mass { get; set; }
+        public double Radius { get; set; }
+        public double CalcFirtCosmVel()
+        {
+            return Math.Sqrt(6.67 * Math.Pow(10, -11) * this.Mass / this.Radius);
+        }
+        public double CalcSecndCosmVel()
+        {
+            return Math.Sqrt(2 * 6.67 * Math.Pow(10, -11) * this.Mass / this.Radius);
+        }
+    }
+}
